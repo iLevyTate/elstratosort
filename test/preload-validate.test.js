8ba3ad6a6c2ddba43ef35ace3a841af1ac75d2ef
Levy Tate <78818969+iLevyTate@users.noreply.github.com>
2025-12-27 13:58:06 -0500

    chore: remove obsolete preload test files
    - Remove preload-sanitize.test.js
    - Remove preload-validate.test.js

--- test/preload-validate.test.js
+++
@@ -1,8 +0,0 @@
-const { SecureIPCManager } = require('../src/preload/preload');
-
-describe('preload: SecureIPCManager channel validation', () => {
-  test('blocks unauthorized channel', async () => {
-    const mgr = new SecureIPCManager();
-    await expect(mgr.safeInvoke('unknown-channel')).rejects.toThrow('Unauthorized IPC channel');
-  });
-});
